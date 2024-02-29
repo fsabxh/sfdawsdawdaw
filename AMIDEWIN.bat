5714fe7053e2c918c9095f16fd9f1d380c202640
loistupidpet <115026895+loistupidpet@users.noreply.github.com>
2024-02-29 11:50:25 -0700

    Add files via upload

---
+++ AMIDEWIN.bat
@@ -0,0 +1,46 @@
+@echo off
+cls
+
+:: Example %RANDOM%-LEPROXY
+
+TITLE AMIDEWIN Windows x64
+
+AMIDEWINx64.exe /IVN %RANDOM%-%RANDOM%
+amidewinx64.exe /IV %RANDOM%-%RANDOM%
+amidewinx64.exe /IV %RANDOM%-%RANDOM%
+amidewinx64.exe /SM %RANDOM%-%RANDOM%
+amidewinx64.exe /SP %RANDOM%-%RANDOM%
+amidewinx64.exe /SV %RANDOM%-%RANDOM%
+amidewinx64.exe /SS %RANDOM%-%RANDOM%
+amidewinx64.exe /SU AUTO
+amidewinx64.exe /SK %RANDOM%-%RANDOM%
+amidewinx64.exe /SF %RANDOM%-%RANDOM%
+amidewinx64.exe /BM %RANDOM%-%RANDOM%
+amidewinx64.exe /BP %RANDOM%-%RANDOM%
+amidewinx64.exe /BV %RANDOM%-%RANDOM%
+amidewinx64.exe /BS %RANDOM%-%RANDOM%
+amidewinx64.exe /BT %RANDOM%-%RANDOM%
+amidewinx64.exe /BLC %RANDOM%-%RANDOM%
+amidewinx64.exe /CM %RANDOM%-%RANDOM%
+amidewinx64.exe /CV %RANDOM%-%RANDOM%
+amidewinx64.exe /CS %RANDOM%-%RANDOM%
+amidewinx64.exe /CA %RANDOM%-%RANDOM%
+amidewinx64.exe /CSK %RANDOM%-%RANDOM%
+amidewinx64.exe /PSN %RANDOM%-%RANDOM%
+amidewinx64.exe /PAT %RANDOM%-%RANDOM%
+amidewinx64.exe /PPN %RANDOM%-%RANDOM%
+amidewinx64.exe /OS 1 %RANDOM%-%RANDOM%
+amidewinx64.exe /OS 2 %RANDOM%-%RANDOM%
+amidewinx64.exe /OS 3 %RANDOM%-%RANDOM%
+amidewinx64.exe /OS 4 %RANDOM%-%RANDOM%
+amidewinx64.exe /OS 5 %RANDOM%-%RANDOM%
+amidewinx64.exe /OS 6 %RANDOM%-%RANDOM%
+amidewinx64.exe /OS 7 %RANDOM%-%RANDOM%
+amidewinx64.exe /OS 8 %RANDOM%-%RANDOM%
+amidewinx64.exe /OS 9 %RANDOM%-%RANDOM%
+amidewinx64.exe /OS 10 %RANDOM%-%RANDOM%
+amidewinx64.exe /OS 11 %RANDOM%-%RANDOM%
+
+cls
+net stop winmgmt /Y >nul
+:: Network Reset
